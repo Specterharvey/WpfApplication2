0c4b3455f3d6a3d4eca277b9ac258dc40e9b007c
Nathiel P de Barros <nathiellbarros@outlook.com>
2016-03-06 00:48:57 -0300

    Project Test

---
+++ WpfApplication2/Models/Calculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2.Models
+{
+    public class Calculo
+    {
+        public string Calcular(string nome)
+        {
+            string a;
+            a = nome.Length.ToString();
+            return a;
+        }
+        #region ReverterNome
+        //public string Reverter(string nome)
+        //{
+        //    char[] array = nome.ToCharArray();
+        //    Array.Reverse(array);
+        //    return new string(array);
+        //}
+        #endregion
+
+
+    }
+}
